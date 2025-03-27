dd1e09e8582e92061607009b45b39eb8cec65894
zlchtl <ebengrad1@mail.ru>
2025-03-27 17:52:37 +0300

    test: add API test script for image conversion endpoint

--- test.py
+++ test.py
@@ -1,8 +1,28 @@
-import os
+import requests
+import json
 
-#скрипт для автотестирования эндпоинтов
-print("\n\n>>> curl.exe -X GET http://127.0.0.1:8080/about")
-os.system("curl.exe -X GET http://127.0.0.1:8080/about")
+url = "http://localhost:8080/convert"
 
-print("\n\n>>> curl.exe -X POST http://localhost:8080/convert -H \"Content-Type: application/octet-stream\" --data-binary \"@images.jpg\"")
-os.system("curl.exe -X POST http://localhost:8080/convert -H \"Content-Type: application/octet-stream\" --data-binary \"@images.jpg\"")
+def test_convert(image_path, size, charset):
+    with open(image_path, 'rb') as img_file:
+        files = {
+            'file': img_file,
+            'params': (None, json.dumps({'size': size, 'charSet': charset}), 'application/json')  # Параметры
+        }
+
+        response = requests.post(url, files=files)
+
+        print(f"Testing {image_path} with size {size} and charset '{charset}'")
+        print(f"Status Code: {response.status_code}")
+        ascii = response.json()["ascii"]
+        print(len(ascii.split("\n")[0]))
+        print(ascii)
+        print('-' * 50)
+        return ascii
+
+def save(s):
+    with open("temp.txt","w") as f:
+        f.write(s)
+
+test_convert('images.jpg', 300, "@%#*+=-:. ")
+save(test_convert('images.png', 300, "@%#*+=-:. "))
